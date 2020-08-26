08da1b5ff7a3abeedba43baa5f54299098a681a5
Natalia <natalia@mail.com>
2020-08-27 00:46:21 +0200

    Updates

---
+++ precache-manifest.ba9bbb60e38105ebf0454fe6fde457b3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed64676c2c5e2db7c951e67090670557",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "27e4d2ff1c1b976357fd",
+    "url": "/weather_app/static/css/main.5248cb35.chunk.css"
+  },
+  {
+    "revision": "659fe7b3a0cdfaad5848",
+    "url": "/weather_app/static/js/2.839d080a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather_app/static/js/2.839d080a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e4d2ff1c1b976357fd",
+    "url": "/weather_app/static/js/main.05a1a2b0.chunk.js"
+  },
+  {
+    "revision": "fd188dc03bd9b22903a1",
+    "url": "/weather_app/static/js/runtime-main.d4472664.js"
+  },
+  {
+    "revision": "fb7567a19260e90446411590badb47ae",
+    "url": "/weather_app/static/media/002-cloud-1.fb7567a1.png"
+  },
+  {
+    "revision": "b327616e9c76ea8fdb1da483e82b6e8d",
+    "url": "/weather_app/static/media/003-rainy.b327616e.png"
+  },
+  {
+    "revision": "e6217ecbe3adecd9daa04599d6236d9c",
+    "url": "/weather_app/static/media/004-rainy-1.e6217ecb.png"
+  },
+  {
+    "revision": "68b96a7935475811e7fc3908c60724d0",
+    "url": "/weather_app/static/media/006-snowy.68b96a79.png"
+  },
+  {
+    "revision": "542c94b842a4ada877ef270e1dd45435",
+    "url": "/weather_app/static/media/011-cloudy.542c94b8.png"
+  },
+  {
+    "revision": "a6fe3e27f0e0834eb2918945489fe495",
+    "url": "/weather_app/static/media/013-storm-2.a6fe3e27.png"
+  },
+  {
+    "revision": "9dd3a945df323943fe962921393d4f05",
+    "url": "/weather_app/static/media/021-night-2.9dd3a945.png"
+  },
+  {
+    "revision": "a29bb72e2073f19835e060fdb4b1e3ef",
+    "url": "/weather_app/static/media/022-night-3.a29bb72e.png"
+  },
+  {
+    "revision": "2c5926b1a46dfeed2cc80266cabc4efa",
+    "url": "/weather_app/static/media/038-cloudy-3.2c5926b1.png"
+  },
+  {
+    "revision": "c2fac50a39696946435392213fbdce5f",
+    "url": "/weather_app/static/media/039-sun.c2fac50a.png"
+  },
+  {
+    "revision": "a3d8c3a6b27ef1c310e21d831d87160c",
+    "url": "/weather_app/static/media/mountains-3840x2160.a3d8c3a6.jpg"
+  }
+]);
